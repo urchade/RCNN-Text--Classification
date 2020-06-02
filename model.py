0d93bd3d5dc3d28e1540eb66f677b7a06fb12251
uzaratiana <uzaratiana@groupeastek.fr>
2020-06-02 23:59:44 +0200

    Model update

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ class RCNN(nn.Module):
         self.linear_2 = nn.Linear(emb_dim + 2 * c_size, h_size)
         self.linear_out = nn.Linear(h_size, out_size)
 
-        self._init = torch.zeros(size=(2, c_size))
+        self._init = nn.Parameter(torch.zeros(size=(2, c_size)), requires_grad=False)
 
     def forward(self, seq_ids):
         out_emb = self.embedding(seq_ids)  # (batch_size, seq_len, emb_dim)
@@ -25,14 +25,11 @@ class RCNN(nn.Module):
 
         cl_wi, cr_wi = self._init
 
-        cl = []
+        cl, cr = [], []
         for i in range(seq_len):
             cl_wi = torch.tanh(cl_wi @ self.W_l + out_emb[:, i, :] @ self.W_sl)
+            cr_wi = torch.tanh(cr_wi @ self.W_r + out_emb[:, seq_len - 1 - i, :] @ self.W_sr)
             cl.append(cl_wi)
-
-        cr = []
-        for i in reversed(range(seq_len)):
-            cr_wi = torch.tanh(cr_wi @ self.W_r + out_emb[:, i, :] @ self.W_sr)
             cr.append(cr_wi)
 
         cl = torch.stack(cl).transpose(0, 1)  # (batch_size, seq_len, c_size)
